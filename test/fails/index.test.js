90b22c3696694b7ea161e76d898878bcddff1371
dword <sebastianlandw@gmail.com>
2019-10-31 21:41:05 +0000

    fix test

--- test/fails/index.test.js
+++ test/fails/index.test.js
@@ -4,7 +4,7 @@ const expect = require('expect')
 it('fails', done => {
   spawn('mocha-per-file', [], { capture: ['stdout'], cwd: __dirname })
     .catch(({ stdout }) => {
-      expect(stdout).toMatch(/^\n\n  a\n    1\) foo\n\n\n  0 passing \(.*?\)\n  1 failing\n\n  1\) a\n       foo:\n     Error: expect\(received\)\.toEqual\(expected\) \/\/ deep equality\n\nExpected: 2\nReceived: 1\n      at Context\.module\.exports \(test\/a\/foo\.test\.js:3:34\)\n\n\n\n$/)
+      expect(stdout).toMatch(/^\n\n  a\n    1\) foo\n\n\n  0 passing \(.*?\)\n  1 failing\n\n  1\) a\n       foo:\n     Error: expect\(received\)\.toEqual\(expected\) \/\/ deep equality\n\nExpected: 2\nReceived: 1/)
       done()
     })
 }).timeout(5000)
