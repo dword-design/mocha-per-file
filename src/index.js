420a227a4343f74cdbf9cb4b34d1e0ed427cd9bc
Sebastian Landwehr <landwehr@zebralog.de>
2019-11-14 13:11:23 +0100

    fix chdir filter bug

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ let args = process.argv.slice(2)
 const isChdir = args.includes('--chdir')
 
 if (isChdir) {
-  args = args.filter(arg => arg === '--chdir')
+  args = args.filter(arg => arg !== '--chdir')
 }
 
 module.exports = () => spawn('mocha', [require.resolve('./test'), ...args], { stdio: 'inherit', env: { ...process.env, IS_CHDIR: isChdir } })
