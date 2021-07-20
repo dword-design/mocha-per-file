e0cf297c241ac6632393642470e1446eca1f00a1
dword <info@sebastianlandwehr.com>
2021-07-20 06:51:30 +0000

    fix: replace forEach with for of

--- src/test.js
+++ src/test.js
@@ -8,41 +8,40 @@ const globalPath = process.env.MOCHA_PER_FILE_PATH
 
 const pattern = process.env.MOCHA_PER_FILE_PATTERN
 
-const rec = (path = globalPath) =>
-  globby
-    .sync('*', { cwd: path, onlyFiles: false })
-    .sort()
-    .forEach(subpath => {
-      const absolutePath = join(path, subpath)
+const rec = (path = globalPath) => {
+  const subpaths = globby.sync('*', { cwd: path, onlyFiles: false }).sort()
+  for (const subpath of subpaths) {
+    const absolutePath = join(path, subpath)
 
-      const relativePath = relative(globalPath, absolutePath)
-      if (lstatSync(absolutePath).isDirectory()) {
-        global.describe(subpath, () => rec(absolutePath))
-      } else if (
-        subpath.endsWith('.test.js') &&
-        minimatch(relativePath, pattern)
-      ) {
-        const testModule = require(resolve(absolutePath))
+    const relativePath = relative(globalPath, absolutePath)
+    if (lstatSync(absolutePath).isDirectory()) {
+      global.describe(subpath, () => rec(absolutePath))
+    } else if (
+      subpath.endsWith('.test.js') &&
+      minimatch(relativePath, pattern)
+    ) {
+      const testModule = require(resolve(absolutePath))
 
-        const handler =
-          typeof testModule === 'function' ? testModule : testModule.it
+      const handler =
+        typeof testModule === 'function' ? testModule : testModule.it
 
-        const timeout =
-          typeof testModule === 'function' ? undefined : testModule.timeout
+      const timeout =
+        typeof testModule === 'function' ? undefined : testModule.timeout
 
-        const only = typeof testModule === 'function' ? false : testModule.only
+      const only = typeof testModule === 'function' ? false : testModule.only
 
-        const itOrOnly = only ? global.it.only : global.it
+      const itOrOnly = only ? global.it.only : global.it
 
-        const test = itOrOnly(
-          basename(subpath, '.test.js'),
-          process.env.MOCHA_PER_FILE_IS_CHDIR === 'true'
-            ? () => chdir(dirname(absolutePath), handler)
-            : handler
-        )
-        if (timeout !== undefined) {
-          test.timeout(timeout)
-        }
+      const test = itOrOnly(
+        basename(subpath, '.test.js'),
+        process.env.MOCHA_PER_FILE_IS_CHDIR === 'true'
+          ? () => chdir(dirname(absolutePath), handler)
+          : handler
+      )
+      if (timeout !== undefined) {
+        test.timeout(timeout)
       }
-    })
+    }
+  }
+}
 rec()
